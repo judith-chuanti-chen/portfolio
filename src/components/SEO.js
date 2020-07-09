6260e2672e607be9dc7ee4d395a3376170de8d73
Judith Chuan Ti Chen <chen.chuan@husky.neu.edu>
2020-07-09 16:57:44 -0700

    Updated SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -30,18 +30,18 @@ const SEO = ({title, description}) => {
     <meta name="description" content={description || siteDesc} />
     <meta name="image" content={image}/>
     <meta name="author" content={author} />
-    <meta property="og:title" content={title|| siteTitle}/>
+    <meta property="og:title" content={title || siteTitle}/>
     <meta property="og:type"  content="website"/>
     <meta property="og:description" content={description || siteDesc} />
-    <meta property="og:image" content={image}/>
+    <meta property="og:image" content={`${siteUrl} ${image}`}/>
     <meta property="og:image:width" content="1200"/>
     <meta property="og:image:height" content="630"/>
     <meta property="og:url" content={siteUrl}/>
     <meta property="fb:app_id" content="276206693457455"/>
     <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:image" content={`${siteUrl} ${image}`}/>
     <meta name="twitter:creator" content={twitterUsername}/>
     <meta name="twitter:title" content={siteTitle}/>
+    <meta name="twitter:image" content={`${siteUrl} ${image}`}/>
     <meta name="twitter:description" content={siteDesc}/>
 
   </Helmet>
